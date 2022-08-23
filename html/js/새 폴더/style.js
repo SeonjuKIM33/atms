87ef255b3d312ae6d5e89f6f0bf64ca4fa78465a
SeonjuKIM33 <107982714+SeonjuKIM33@users.noreply.github.com>
2022-08-24 02:04:15 +0900

    Add files via upload

---
+++ html/js/새 폴더/style.js
@@ -0,0 +1,44 @@
+jQuery(function(){
+	//snb
+	/*$('#side a').parents('ul').prev('a').addClass('child_dep');
+	$('.side_menu .dep3').prev('a').on('click',function(event){
+		var $target=$(event.target);
+		if($target.is('.on')){
+			$('.side_menu .dep1 > li > a').removeClass('on').next('ul').removeClass('dep2_on').find('a').removeClass('on').next('ul').removeClass('dep3_on');
+		}else{
+			$('.side_menu .dep1 > li > a').removeClass('on').next('ul').removeClass('dep2_on').find('a').removeClass('on').next('ul').removeClass('dep3_on');
+			$(this).addClass('on').next('ul').addClass('dep3_on')
+		};
+		return false;
+	});
+	$('.side_menu .dep2').prev('a').on('click',function(event){
+		var $target=$(event.target);
+		if($target.is('.on')){
+			$('.side_menu .dep1 > li > a').removeClass('on').next('ul').removeClass('dep2_on').find('a').removeClass('on').next('ul').removeClass('dep3_on');
+		}else{
+			$('.side_menu .dep1 > li > a').removeClass('on').next('ul').removeClass('dep2_on').find('a').removeClass('on').next('ul').removeClass('dep3_on');
+			$(this).addClass('on').next('ul').addClass('dep2_on')
+		};
+		return false;
+	});*/
+
+	var focus_button;
+	$('.bbs_popup_open').on('click',function(event){
+		focus_button=$(event.target);
+		$('body').addClass('bbs_popup_active');
+	});
+	$('.bbs_popup .close').on('click',function(){
+		$('body').removeClass('bbs_popup_active');
+		focus_button.focus();
+	});
+});
+
+/*function side_menu(fir,sec,thi){
+	if(typeof(thi)=='undefined'){
+		$('.side_menu > ul > li:nth-child('+fir+') > a').addClass('on2').next('ul').addClass('current').find('> li:nth-child('+sec+') > a').addClass('on2');
+	}else if(typeof(sec)=='undefined'){
+		$('.side_menu > ul > li:nth-child('+fir+') > a').addClass('on2');
+	}else{
+		$('.side_menu > ul > li:nth-child('+fir+') > a').addClass('on2').next('ul').addClass('current').find('> li:nth-child('+sec+') > a').addClass('on2').next('ul').addClass('current').find('li:nth-child('+sec+') a').addClass('on2');
+	};
+};*/
